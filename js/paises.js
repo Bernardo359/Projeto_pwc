bc886518748475269a5c66f36b0b62d1258cd659
Bernardo359 <2232337@my.ipleiria.pt>
2024-12-22 15:24:19 +0000

    API att

---
+++ js/paises.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    var CloneOriginalCard = $('.card.d-none').clone().removeClass('d-none');
+
+    $('.lista-paises').html('');
+
+    $.ajax({
+        method: "GET",
+        url: "https://restcountries.com/v3.1/all",
+    }).done(function (dados) {
+
+        for (var i = 0; i < dados.length; i++) {
+            var clonecard = CloneOriginalCard.clone(); 
+
+            $('.card-image', clonecard).attr('src', dados[i].flags.png);
+            $('.card-info h2', clonecard).text(dados[i].name.common);
+
+            $('.lista-paises').append(clonecard);
+        }
+    }).fail(function (error) {
+        console.error('Erro ao buscar os dados da API:', error);
+    });
+});
